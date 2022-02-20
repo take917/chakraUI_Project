0dcd7583adab2b03c8fe10bb2254819a221898cf
take917 <35625934+take917@users.noreply.github.com>
2022-02-20 13:26:16 +0900

    ログイン部分のコンポーネント化 ログインのボタン部分をコンポーネント化

---
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@chakra-ui/react";
+import { VFC, memo, ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const PrimaryButton: VFC<Props> = memo((props) => {
+  const { children } = props;
+  return (
+    <>
+      <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+        {children}
+      </Button>
+    </>
+  );
+});
--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,14 +1,8 @@
-import {
-  Box,
-  Button,
-  Divider,
-  Flex,
-  Heading,
-  Input,
-  Stack
-} from "@chakra-ui/react";
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import { VFC } from "react";
 import { memo } from "react";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+
 export const Login: VFC = memo(() => {
   return (
     <Flex align="center" justify="center" height="100vh">
@@ -19,9 +13,7 @@ export const Login: VFC = memo(() => {
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
           <Input placeholder="ユーザーID" />
-          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
-            ログイン
-          </Button>
+          <PrimaryButton>ログイン</PrimaryButton>
         </Stack>
       </Box>
     </Flex>
