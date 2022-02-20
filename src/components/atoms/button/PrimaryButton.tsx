a33792935316695068837e957539b087b3874ebd
take917 <35625934+take917@users.noreply.github.com>
2022-02-20 18:00:57 +0900

    login認証 longin認証の実装 axiosでの連動

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -3,13 +3,23 @@ import { VFC, memo, ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
+  disabled?: boolean;
+  loading?: boolean;
+  onClick: () => void;
 };
 
 export const PrimaryButton: VFC<Props> = memo((props) => {
-  const { children } = props;
+  const { children, disabled = false, loading = false, onClick } = props;
   return (
     <>
-      <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+      <Button
+        bg="teal.400"
+        color="white"
+        _hover={{ opacity: 0.8 }}
+        disabled={disabled || loading}
+        loading={loading}
+        onClick={onClick}
+      >
         {children}
       </Button>
     </>
